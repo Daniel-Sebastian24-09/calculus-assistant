a42f5e2d2805893e49a0ddc0467aff053f8f2db8
Daniel Sebastian Nunes Cavalcanti <danielsnc240905@gmail.com>
2022-03-01 18:07:56 -0300

    list add compact

--- calculations/math/bascara-theorem/bascara-theorem.py
+++ calculations/math/bascara-theorem/bascara-theorem.py
@@ -4,13 +4,13 @@ b = float(input('Type b: '))
 c = float(input('Type c: '))
 
 delta = b**2 - 4 * a * c
-
+bascaraTheoremX= [0, 0, 0]
 if delta == 0:
-    bascaraTheorem = -b / (2 * a)
-    print(f'x1 = x2 = {bascaraTheorem:.2f}')
+    bascaraTheoremX[0] = -b / (2 * a)
+    print(f'x1 = x2 = {bascaraTheoremX[0]:.2f}')
 if delta > 0:
-    bascaraTheoremX1 = (-b + sqrt(delta))/ (2 * a)
-    bascaraTheoremX2 = (-b - sqrt(delta))/ (2 * a)
-    print(f'x1 = {bascaraTheoremX1:.2f}, and x2 = {bascaraTheoremX2:.2f}')
+    bascaraTheoremX[1] = (-b + sqrt(delta))/ (2 * a)
+    bascaraTheoremX[2] = (-b - sqrt(delta))/ (2 * a)
+    print(f'x1 = {bascaraTheoremX[1]:.2f}, and x2 = {bascaraTheoremX[2]:.2f}')
 if delta < 0:
     print(f"It's not possibible, because delta = {delta} ")
